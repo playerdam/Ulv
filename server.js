bd59c2ed06cd4b1915ec22e61e9f2c72384d28d8
playerdam <fdnnielsen@gmail.com>
2026-05-21 18:53:49 +0200

    Delete server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.static(__dirname));
-app.listen(PORT, () => console.log(`Kører på port ${PORT}`));
